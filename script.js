6d44efb775e0a43fa1fea756eacb7f13f5f8c71b
mika-alpha <yuukeikioku@gmail.com>
2022-07-13 23:42:54 -0500

    Add start button and players

--- script.js
+++ script.js
@@ -2,14 +2,30 @@
 
 
  const game = (() => {
+    const startButton = document.getElementsByClassName('start-button')[0];
     const playerNames = document.getElementsByClassName('player-name');
-    
+    let playerOne;
+    let playerTwo;
+
+    const startGame = () => {
+        startButton.style.display = "none";
+        createPlayers();
+        for(let i = 0; i < 2; i++){
+            playerNames[i].disabled = true;
+        }
+    }
+
+    const createPlayers = () => {
+        playerOne = Player(playerNames[0].value, 'X');
+        playerTwo = Player(playerNames[1], 'O');
+    }
+    startButton.addEventListener('click', startGame);
 
  })();
 
 const gameBoard = (() => {
     const gameGrid = document.getElementsByClassName('game-grid')[0];
-    const gameCells = Array.from(getElementsByClassName('game-cell'));
+    const gameCells = Array.from(document.getElementsByClassName('game-cell'));
     const gameBoard = [];
 
 })();
